9ba302dd183905fc9f929df2df8ba9ac75891071
Claudiano Alves <136628797+Claudiano-alves@users.noreply.github.com>
2024-04-30 18:55:44 -0300

    Delete Projeto_API directory

--- Projeto_API/Projeto_API/modelos/Plano.cs
+++
@@ -1,9 +0,0 @@
-public class Plano
-{
-    public int Id { get; set; }
-    public string plano { get; set; }
-    public int valor { get; set; }
-    public string Nome { get; set; }
-
-    public Usuario? Usuario { get; set; }
-}
